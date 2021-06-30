4a99e819e1451c83026504dad4a9327a6801675f
vino <vino0908@outlook.com>
2021-06-30 11:00:02 +0800

    initail

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <Desktop></Desktop>
+</template>
+
+<script lang="ts">
+import {defineComponent} from 'vue';
+import Desktop from "./components/Desktop.vue";
+
+export default defineComponent({
+  name: 'App',
+  components: {Desktop}
+});
+</script>
+
+<style>
+</style>
---
+++ src/components/Desktop.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="desktop-wrapper">
+    <main class="desktop-main">desktop</main>
+    <nav class="desktop-nav">
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </nav>
+  </div>
+</template>
+<script lang="ts">
+import {defineComponent} from 'vue';
+
+export default defineComponent({
+  name: 'Desktop'
+});
+</script>
+<style lang="scss" scoped>
+.desktop {
+  &-wrapper {
+    width: 100vw;
+    height: 100vh;
+    background: url("/src/assets/wallet.jpg") center;
+    display: flex;
+    flex-direction: column;
+  }
+
+  &-main {
+    flex-grow: 1;
+  }
+
+  &-nav {
+    border-radius: 8px;
+    width: 100%;
+    height: 5vh;
+    background: #d6dff4;
+    opacity: .8;
+  }
+}
+</style>
