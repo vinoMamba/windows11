69efb74c1a1d8146403ed329cc21019ec20d3069
vino <vino0908@outlook.com>
2021-07-05 16:49:21 +0800

    fix: 增加是否有getBattery方法的判断

--- src/hooks/useBattery.ts
+++ src/hooks/useBattery.ts
@@ -13,33 +13,35 @@ type NavigatorWithBattery = Navigator & {
 
 
 export function useBattery() {
+    const hasBattery = navigator && 'getBattery' in navigator;
     //电量
     const level = ref(1);
     // 是否正在充电
-    const charging = ref(false);
+    const charging = ref(true);
     // 充满电需要多少秒,0 表示充电完成
     const chargingTime = ref(0);
     // 充满电需要多少秒,0 表示充电完成
     const dischargingTime = ref(0);
-
-    (navigator as NavigatorWithBattery).getBattery().then(battery => {
-        level.value = battery.level;
-        charging.value = battery.charging;
-        chargingTime.value = battery.chargingTime;
-        dischargingTime.value = battery.dischargingTime;
-        battery.addEventListener('chargingchange', () => {
-            charging.value = battery.charging;
-        });
-        battery.addEventListener('chargingtimechange', () => {
-            chargingTime.value = battery.chargingTime;
-        });
-        battery.addEventListener('dischargingtimechange', () => {
-            dischargingTime.value = battery.dischargingTime;
-        });
-        battery.addEventListener('levelchange', () => {
+    if (hasBattery) {
+        (navigator as NavigatorWithBattery).getBattery().then(battery => {
             level.value = battery.level;
+            charging.value = battery.charging;
+            chargingTime.value = battery.chargingTime;
+            dischargingTime.value = battery.dischargingTime;
+            battery.addEventListener('chargingchange', () => {
+                charging.value = battery.charging;
+            });
+            battery.addEventListener('chargingtimechange', () => {
+                chargingTime.value = battery.chargingTime;
+            });
+            battery.addEventListener('dischargingtimechange', () => {
+                dischargingTime.value = battery.dischargingTime;
+            });
+            battery.addEventListener('levelchange', () => {
+                level.value = battery.level;
+            });
         });
-    });
+    }
 
     return {level, charging, chargingTime, dischargingTime};
 }
