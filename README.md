9779cf7b5438c9f84ba516ce9b59131ee6ee859c
vino <vino0908@outlook.com>
2021-10-20 15:39:50 +0800

    update README.md

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 <h1 align="center">🚀 Windows 11 by Vino</h1>
 
-[预览地址](www.vinomamba.com)
+[预览地址](http://101.34.105.102:3324/windows11/index.html)
+
+- 使用了 Vite +Vue3.0 ，实践了如何在 Vuex 项目中封装 Vue Hooks
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
-import {defineConfig} from 'vite';
-import vue from '@vitejs/plugin-vue';
-import {svgBuilder} from "./src/plugins/svgBuilder";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { svgBuilder } from "./src/plugins/svgBuilder";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue(), svgBuilder('./src/assets/icons/')]
+  plugins: [vue(), svgBuilder("./src/assets/icons/")],
+  base: "/windows11",
 });
